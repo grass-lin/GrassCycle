68c68097ca95c755ab309b97322429657ad33f23
lingrass <lingrass>
2024-08-03 15:37:57 +0800

    Start FrontEnd and add fundamental UIs

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App
+  },
+  {
+    path: "/login",
+    //Componment: LoginPage
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+    </RouterProvider>
+  </React.StrictMode>,
+)
